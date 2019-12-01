387bb81eae7885e5b59011ae1e38f97a34cee87f
Allan Amaral <allanmaral@hotmail.com>
2019-12-01 20:55:38 -0300

    Updates

---
+++ precache-manifest.ccbf30e15a44d4a8835a27ad64bcdf8d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0bd69e38f51f9c1c9447aacad11e11",
+    "url": "/master-heroes/index.html"
+  },
+  {
+    "revision": "a3571211d60a399b724f",
+    "url": "/master-heroes/static/js/2.6e70f595.chunk.js"
+  },
+  {
+    "revision": "f990581f8ed40ae152b8",
+    "url": "/master-heroes/static/js/main.9678fa37.chunk.js"
+  },
+  {
+    "revision": "22da0072cc8393dccbed",
+    "url": "/master-heroes/static/js/runtime-main.f10a2bc7.js"
+  }
+]);
